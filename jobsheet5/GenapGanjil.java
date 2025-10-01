273a4a98e3a2fe14203ff5043101246356657476
Sakti Cakra <saktisakti8451@gmail.com>
2025-10-01 12:25:34 +0700

    tugas 2

---
+++ jobsheet5/GenapGanjil.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class GenapGanjil {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan angka: ");
+        int angka = scanner.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+    }
+}
